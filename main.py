9c35bfdbe9cf5e83e5f197f3ebc75922a4ee6b01
Dino <rlacksdnd951@gmail.com>
2024-10-29 14:41:29 +0900

    캘리포니아 집 예측 서비스 개발

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 # streamlit에 쓸 파일
 # ml은 colab
 import streamlit as st
+from sklearn.ensemble import RandomForestRegressor
+import numpy as np
+import pickle
 
 st.title("MachineLearning")
 # git switch -c 브랜치명 (브랜치 생성하면서 바로 그 브랜치로 넘어간다)
-st.header("네이버")
+st.header("캘리포니아 집값 예측 프로그램")
+cols = [ 'MedInc','HouseAge','AveRooms','AveBedrms', 'Population','AveOccup','Latitude','Longitude']
+datas = [0]*8
+for idx, col in enumerate(cols):
+    data[idx] = st.number_input(col)
+x = np.array(datas).reshape(1, -1)
+x.shape
+
+with open("rf_housing.pickle", "rb") as f:
+    rf_model = pickle.load(f)
+
+y = rf_model.predict(x)
 
 # https://github.com/likelionbe12/lion_machinelearning.git 포크
 # 여러분의 깃허브로 가셔서 lion_machinelearning 레포를 확인
